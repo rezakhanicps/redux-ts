71df48d93ef5056e7c5197ae88c1e7bde50a65c8
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-09-25 17:05:13 +0330

    Create repositoriesReducer.ts

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,59 @@
+interface RepositioriesState {
+    loading: boolean;
+    error: string | null;
+    data: string[];
+}
+
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+interface SearchRepositoriesAction {
+    type: "search_repositories";
+}
+
+interface SearchRepositoriesSuccessAction {
+    type: "search_repositories_success";
+    payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+    type: "search_repositories_error";
+    payload: string;
+}
+
+const reducer = (
+    state: RepositioriesState,
+    action:
+        | SearchRepositoriesAction
+        | SearchRepositoriesSuccessAction
+        | SearchRepositoriesErrorAction
+): RepositioriesState => {
+    switch (action.type) {
+        case "search_repositories":
+            return {
+                loading: true,
+                error: null,
+                data: [],
+            };
+        case "search_repositories_success":
+            return {
+                loading: false,
+                error: null,
+                data: action.payload,
+            };
+
+        case "search_repositories_error":
+            return {
+                loading: false,
+                error: action.payload,
+                data: [],
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
