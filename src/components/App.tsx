daaaa3da932d7d3a3d30b10dd29363c55429115b
reza khani <43088060+rezakhani1376@users.noreply.github.com>
2022-09-25 19:23:03 +0330

    combine reducers

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import { Provider } from "react-redux";
+import { store } from "../state";
+
+const App = ()=>{
+
+    return <Provider store={store}>
+        <div> lkdfhg</div>
+    </Provider>
+}
+
+
+
+export default App;
---
+++ src/state/reducers/index.ts
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import repositoriesReducer from "./repositoriesReducer";
+
+
+
+const reducers = combineReducers({
+    repositories: repositoriesReducer,
+
+})
+
+
+export default reducers
